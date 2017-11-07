2147450b3e9b487fd2ab94bcea981122c3ffac39
phelipe <phelipe@task.com.br>
2017-11-07 12:32:10 -0200

    first commit

---
+++ resources/views/welcome.haml.blade.php
@@ -0,0 +1,10 @@
+!!!
+%html
+  %head
+    %meta{:charset => "utf-8"}/
+    %meta{:content => "IE=edge", "http-equiv" => "X-UA-Compatible"}/
+    %meta{:content => "width=device-width, initial-scale=1", :name => "viewport"}/
+    %title Phelipe E. S. Diniz
+
+  %body
+    oi
