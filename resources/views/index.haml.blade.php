e2ae36c8103446e4d16beeb32d5f6307a4ede942
phelipe <phelipe@task.com.br>
2017-11-08 11:10:30 -0200

    iniciando layout do portfolio

---
+++ resources/views/index.haml.blade.php
@@ -0,0 +1,15 @@
+!!!
+%html
+  %head
+    %meta{:charset => "utf-8"}
+    %meta{:content => "IE=edge", "http-equiv" => "X-UA-Compatible"}
+    %meta{:content => "width=device-width, initial-scale=1", :name => "viewport"}
+    %title Phelipe E. S. Diniz
+    -# FavIcon
+    -# %link{:href => "/favicon.ico", :rel => "icon", :type => "image/x-icon"}
+
+    -# CSS
+    %link{:href => "/css/theme-default.css", :rel => "stylesheet", :type => "text/css"}
+
+  %body
+    @include('topo')
